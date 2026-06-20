807c2540adcafb23cb1429ed41d98eed49bc5487
Hacixan Hacixanov <hajikhanih@code.edu.az>
2026-06-20 14:27:44 +0400

    I Learining react , Topic : react-routing(outlet)

--- REACT/09.react-routing[outlet]/src/layouts/Admin/Header/index.jsx
+++ REACT/09.react-routing[outlet]/src/layouts/Admin/Header/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
-const Header = () => {
+import { NavLink } from 'react-router-dom'
+import './index.scss'
+const AdminHeader = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container">
+        <div className="header">
+          <nav>
+            <ul>
+              <li>
+                <NavLink to={"/admin"} end>Dashboard</NavLink>
+              </li>
+               <li>
+                <NavLink to={"/admin/products"} end>Products</NavLink>
+              </li>
+               <li>
+                <NavLink to={"/admin/products/new"} end>Add Product</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
   )
 }
 
-export default Header
+export default AdminHeader
+
